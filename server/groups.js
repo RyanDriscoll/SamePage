08e82c059bf02498e8afba81bd16ebd6422019ee
KdawgG <kamaldeep.gandhi@gmail.com>
2017-01-25 14:11:05 -0600

    updated routes

--- server/groups.js
+++ server/groups.js
@@ -4,12 +4,25 @@ const db = require('APP/db')
 const Group = db.model('groups')
 
 module.exports = require('express').Router()
-	.get('/', (req, res, next) => 
-		Group.findAll({ 
-			where: {name: req.query.url, url: req.query.url},
+	.get('/url/:url', (req, res, next) => 
+		Group.findOne({ 
+			where: {name: req.params.url, url: req.params.url},
 			include:[User, Message]
 		})
-		.then(groups => res.json(groups))
+		.then(group => res.json(group))
+		.catch(next))
+	.get('/', (req, res, next) =>
+		Group.findOne({
+			where: {name: req.query.name, url: req.query.url},
+			include: [User, Message]
+		})
+		.then(group => res.json(group))
+		.catch(next))
+	.get('/user/:id', (req, res, next) =>
+		Group.findAll({
+			include: [{model: User, where:{ id: req.params.id}}, Message]
+		})
+		.then(group => res.json(group))
 		.catch(next))
 	.post('/', (req, res, next) =>
 		Group.create(req.body)
