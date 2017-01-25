e062d21011b9de5ede85916c18996a5a95b0ce54
Alan H <alherman10@gamil.com>
2017-01-25 15:48:33 -0600

    injected sidebar onto websites

--- app/content/react/maincontainer.jsx
+++ app/content/react/maincontainer.jsx
@@ -4,12 +4,21 @@ import {connect} from 'react-redux';
 class MainContainer extends React.Component{
   constructor(props){
     super(props);
-
+    this.sideBarStyle = {
+      position: 'fixed',
+      right: '0px',
+      top: '0px',
+      width: '30vw',
+      height: '100%',
+      backgroundColor: 'blue',
+      float: 'right',
+      marginLeft: '20px'
+    }
   }
 
   render(){
     return (
-      <div>I just injected you!</div>
+      <div style={this.sideBarStyle}>I just injected you!</div>
     )
   }
 }
