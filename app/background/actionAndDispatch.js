948d8deb79c37d2295902cc78ab6128b252517b9
Alan H <alherman10@gamil.com>
2017-02-04 15:36:51 -0600

    remove user working

--- app/background/actionAndDispatch.js
+++ app/background/actionAndDispatch.js
@@ -92,6 +92,6 @@ export const addGroup = (url, name) => {
 
 export const removeUser = (groupId, userId) => {
   console.log("in removeUser", groupId, userId)
-  axios.delete(rootPath + 'groups/users', {params: {groupId, userId}, data: {groupId, userId}})
+  axios.delete(rootPath + 'groups/users', {data: {groupId, userId}})
   .catch(err => console.log(err, err.stack))
 }
--- server/groups.js
+++ server/groups.js
@@ -51,7 +51,6 @@ module.exports = require('express').Router()
 
 
 	.delete('/users', (req, res, next) => {
-		console.log("----------------- \n", req.config, req.params, req.data, req.body, req.query)
 		GroupUser.destroy({where: {group_id: req.body.groupId, user_id: req.body.userId}})
 		.then(result => {
 			sockets.io.emit('remove:user', {groupId: req.body.groupId, userId: req.body.userId})
