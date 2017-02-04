4a3862890455a2088230ea8f06ef6bb45502f216
Ryan Driscoll <driscollrp@gmail.com>
2017-02-04 16:15:50 -0600

    scroll lock working

--- app/content/react/ChatContainer.jsx
+++ app/content/react/ChatContainer.jsx
@@ -6,22 +6,36 @@ import ReactTransitionGroup from 'react-addons-transition-group';
 import MessageContainer from './messagecontainer.jsx';
 import UserContainer from './UserContainer.jsx';
 import SendMessageComponent from './sendmessagecomponent.jsx';
+import ScrollLock from 'react-scroll-lock-component';
 
 
 class ChatContainer extends React.Component{
   constructor(props){
     super(props);
+
+    this.stopScroll = this.stopScroll.bind(this);
+    this.startScroll = this.startScroll.bind(this);
+  }
+
+  stopScroll() {
+    console.log('stopScroll', document.body)
+    document.body.style.overflow = 'hidden';
+  }
+
+  startScroll() {
+    console.log('startScroll', document.body)
+    document.body.style.overflow = 'scroll';
   }
 
 
 
   render(){
     return (
-        <div className={this.props.animation}>
-          <UserContainer />
+      <div className={this.props.animation} onMouseEnter={this.stopScroll} onMouseLeave={this.startScroll}>
+        <UserContainer />
           <MessageContainer />
-          <SendMessageComponent />
-        </div>
+        <SendMessageComponent />
+      </div>
     );
   }
 }
