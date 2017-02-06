ad40346bc558d4a412605e8a218ac91dd5d42838
Tredis <t.redis@gmail.com>
2017-02-06 11:43:04 -0600

    handle async join

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -29,14 +29,37 @@ module.exports = {
       socket.on('joinGroup', ({url, name, user_id}) => {
         Group.findOrCreate({where: {url, name}})
         .then(([group, created]) => {
-          socket.join(group.id).emit('joinGroup', group);
-          GroupUser.create({user_id, group_id: group.id})
-          User.findById(user_id)
-          .then(user => socket.broadcast.to(group.id).emit('addUser', 
-            {groupId: group.id, row: user, user_id})) //userId
+          socket.join(group.id, err => {
+            if (err) { throw err }
+            socket.emit('joinGroup', group);
+            GroupUser.create({user_id, group_id: group.id})
+            User.findById(user_id)
+            .then(user => socket.broadcast.to(group.id).emit('addUser', 
+              {groupId: group.id, row: user, user_id})) //was userId
+          })
         })
       })
       //remove hook emit, and client reject
+
+      // socket.on('joinGroup', ({url, name, user_id}) => {
+      //   Group.findOrCreate({where: {url, name}})
+      //   .then(([group, created]) => {
+      //     return new Promise((resolve, reject) => {
+      //       socket.join(group.id, err => {
+      //         if(err) { return reject(err); }
+      //         resolve(group);
+      //       })
+      //     })
+      //   })
+      //   .then((group) => {
+      //       socket.emit('joinGroup', group);
+      //       GroupUser.create({user_id, group_id: group.id})
+      //       return User.findById(user_id)
+      //       .then(user => sockets.io.broadcast.in(group.id).emit('addUser', 
+      //         {groupId: group.id, row: user, user_id})) //was userId
+      //     })
+      //   })
+      // })
 
       socket.on('disconnect', () => {
         console.log("disconnected___", socket.id)
