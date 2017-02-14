d7f410df0616c1f5112d23ce0c248b96c08c31ae
Ryan Driscoll <driscollrp@gmail.com>
2017-02-14 15:59:14 -0600

    demo

--- app/content/react/httpServer.js
+++ app/content/react/httpServer.js
@@ -1,5 +1,5 @@
 
-export default 'http://localhost:1337/';
+// export default 'http://localhost:1337/';
 // export default 'http://172.28.117.220:1337/';
-// export default 'https://hidden-ravine-50568.herokuapp.com/';
+export default 'https://hidden-ravine-50568.herokuapp.com/';
 
